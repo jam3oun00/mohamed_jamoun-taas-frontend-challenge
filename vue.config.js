decfd2c71839f429e6d6a46f1a99d27a8fe627da
mohamed jamoun <mjamoun28@gmail.com>
2022-09-08 19:19:50 +0100

    adding .env.local along with vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('@vue/cli-service')
+const Dotenv = require('dotenv-webpack')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new Dotenv({
+        path: './.env.local',
+        safe: true
+      })
+    ]
+  }
+})
