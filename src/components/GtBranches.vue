118358cab995475b5f8dfcd09dc6d0f42389ae1d
mohamed jamoun <mjamoun28@gmail.com>
2022-09-08 23:07:19 +0100

    add branches select-box and other functionalities to the branches component

--- src/components/GtBranches.vue
+++ src/components/GtBranches.vue
@@ -1,11 +1,39 @@
 <template>
-  <div></div>
+  <div class="flex">
+    <select v-model="selectedBranch" class="btn primary-select">
+      <option v-for="branch in branches" :key="branch.name" :value="branch">
+        {{ branch.name }}
+      </option>
+    </select>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, PropType } from 'vue'
+import { Branch } from '@/types'
 
-export default defineComponent({})
+export default defineComponent({
+  props: {
+    branches: {
+      type: Array as PropType<Branch[]>,
+      default: () => []
+    },
+    modelValue: {
+      type: Object as PropType<Branch>,
+      default: () => ({})
+    }
+  },
+  computed: {
+    selectedBranch: {
+      get() {
+        return this.modelValue
+      },
+      set(branch: Branch) {
+        this.$emit('update:modelValue', branch)
+      }
+    }
+  }
+})
 </script>
 
 <style scoped></style>
