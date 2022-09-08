a9339a82aed11c38f7e110295be06a268c3c1d16
mohamed jamoun <mjamoun28@gmail.com>
2022-09-08 20:17:14 +0100

    Adding the authorisation button in the homepage, with ability to redirect to the GitHub authorisation page

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,11 +1,21 @@
 <template>
-  <div class="w-creen h-screen bg-red-500">test</div>
+  <div class="flex items-center justify-center w-screen h-screen">
+    <div class="container flex items-center justify-center">
+      <button class="btn-primary" @click="startGithubAuth">Authorize my GitHub account</button>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-export default defineComponent({})
+export default defineComponent({
+  name: 'home',
+  methods: {
+    startGithubAuth() {
+      // redirect to github oauth
+      window.location.href = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+    }
+  }
+})
 </script>
-
-<style scoped></style>
