b3bd4347f9739aa4d3e24a5e8ac78680859d7974
mohamed jamoun <mjamoun28@gmail.com>
2022-09-09 01:00:36 +0100

    add 404_not_fount page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,25 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 const routes: Array<RouteRecordRaw> = [
   {
-    path: "/",
-    name: "home",
-    component: () => import("@/views/home.vue"),
+    path: '/',
+    name: 'home',
+    component: () => import('@/views/home.vue')
   },
   {
-    path: "/repositories",
-    name: "repositories",
-    component: () => import("@/views/repositories.vue"),
+    path: '/repositories',
+    name: 'repositories',
+    component: () => import('@/views/repositories.vue')
   },
-];
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'not-found',
+    component: () => import('@/views/not-found.vue')
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes,
-});
+  routes
+})
 
-export default router;
+export default router
---
+++ src/views/not-found.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-screen h-screen flex items-center justify-center text-3xl font-bold">
+    404 - Page not found
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({})
+</script>
+
+<style scoped></style>
